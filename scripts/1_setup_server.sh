c5198f9d4244de5dd5dc591eb048a4e112801347
Maximilian Wank <maxi.wank@gmx.de>
2025-02-17 16:55:40 +0100

    Rename security group for WireGuard and add server setup script

---
+++ scripts/1_setup_server.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -ex
+
+# Generate server keys
+wg genkey | tee /etc/wireguard/privatekey | wg pubkey > /etc/wireguard/publickey
+
+# Create WireGuard configuration file
+cat <<EOF > /etc/wireguard/wg0.conf
+[Interface]
+PrivateKey = $(cat /etc/wireguard/privatekey)
+Address = 10.0.0.1/24
+ListenPort = 51820
+
+[Peer]
+PublicKey = $(cat /etc/wireguard/publickey)
+AllowedIPs = 10.0.0.2/24
+EOF
+
+# Enable IP forwarding
+echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
+sysctl -p
+
+# Start and enable WireGuard
+wg-quick up wg0
+systemctl enable wg-quick@wg0
