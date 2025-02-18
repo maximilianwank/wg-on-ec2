0c2180eee74e771caf28eec3d06dbd15353b1277
Maximilian Wank <maxi.wank@gmx.de>
2025-02-18 14:07:09 +0100

    Enhance server setup script to include default interface handling and improve iptables rules for WireGuard

--- scripts/1_setup_server.sh
+++ scripts/1_setup_server.sh
@@ -5,12 +5,17 @@ wg genkey | tee /etc/wireguard/server_private.key | wg pubkey > /etc/wireguard/s
 wg genkey | tee /etc/wireguard/client1_private.key | wg pubkey > /etc/wireguard/client1_public.key
 wg genkey | tee /etc/wireguard/client2_private.key | wg pubkey > /etc/wireguard/client2_public.key
 
+# Get default interface name
+default_if_name=$(ip route | grep '^default' | awk '{print $5}')
+
 # Create WireGuard configuration file
 cat <<EOF > /etc/wireguard/wg0.conf
 [Interface]
 PrivateKey = $(cat /etc/wireguard/server_private.key)
 Address = 10.0.0.1/24
 ListenPort = 51820
+PostUp = echo 1 > /proc/sys/net/ipv4/ip_forward; iptables -A FORWARD -i wg0 -j ACCEPT; iptables -A FORWARD -o wg0 -j ACCEPT; iptables -t nat -A POSTROUTING -s 10.0.0.1./24 -o $default_if_name -j MASQUERADE
+PostDown = echo 0 > /proc/sys/net/ipv4/ip_forward; iptables -D FORWARD -i wg0 -j ACCEPT; iptables -D FORWARD -o wg0 -j ACCEPT; iptables -t nat -D POSTROUTING -s 10.0.0.1./24 -o $default_if_name -j MASQUERADE
 
 [Peer]
 PublicKey = $(cat /etc/wireguard/client1_public.key)
